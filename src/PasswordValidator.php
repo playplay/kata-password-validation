66feacc9e835e425636c58e9641d1a46f1d1ef18
Thomas Calvet <thomas.calvet@playplay.com>
2022-10-26 16:57:09 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:tests/SampleTest.php

--- src/PasswordValidator.php
+++ src/PasswordValidator.php
@@ -7,6 +7,9 @@ class PasswordValidator
     public function validate(string $password): bool
     {
         return strlen($password) > 8
-            && preg_match('/[A-Z]/', $password);
+            && preg_match('/[A-Z]/', $password)
+            && preg_match('/[a-z]/', $password)
+            && preg_match('/[0-9]/', $password)
+            && str_contains($password, '_');
     }
 }
--- tests/SampleTest.php
+++ tests/SampleTest.php
@@ -20,12 +20,28 @@ class SampleTest extends TestCase
 
     public function testPasswordIsValid(): void
     {
-        $this->assertTrue((new PasswordValidator())->validate('A23456789'));
-        $this->assertTrue((new PasswordValidator())->validate('AAAA56789'));
+        $this->assertTrue((new PasswordValidator())->validate('Aa3_56789'));
+        $this->assertTrue((new PasswordValidator())->validate('AAAAa6_89'));
+        $this->assertTrue((new PasswordValidator())->validate('AAaa5_789'));
     }
 
     public function testPasswordWithoutCapitalLetterIsInvalid(): void
     {
         $this->assertFalse((new PasswordValidator())->validate('123456789'));
+    }
+
+    public function testPasswordWithoutLowercaseLetterIsInvalid(): void
+    {
+        $this->assertFalse((new PasswordValidator())->validate('A23456789'));
+    }
+
+    public function testPasswordWithoutANumberIsInvalid(): void
+    {
+        $this->assertFalse((new PasswordValidator())->validate('AaaFOOBAR'));
+    }
+
+    public function testPasswordWithoutUnderscoreIsInvalid(): void
+    {
+        $this->assertFalse((new PasswordValidator())->validate('AaaF00BAR'));
     }
 }
